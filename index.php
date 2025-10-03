22cb62161e7ac62e1f2e592b89e4443315bb4b91
impostercode1992 <mdshariefsalleh@gmail.com>
2025-10-03 15:13:30 +0800

    latest

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 include 'header.html'; // Includes the content of header.html
-?>
-<h1>Welcome to my website!</h1>
-<?php
+include 'body.html';
 include 'footer.html'; // Includes the content of header.html
 ?>
