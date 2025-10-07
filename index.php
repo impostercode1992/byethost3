a5547a01311f271cbd19bf733109afe382e4f392
impostercode1992 <mdshariefsalleh@gmail.com>
2025-10-07 21:02:37 +0800

    mod

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php include 'wc\header.html';?>
 <?php include 'wc\navbar.html';?>
+    <div class="w3-content">
     <?php include 'work.html';?>
     <?php include 'price.html';?>
+    </div>
 <?php include 'wc\footer.html';?>
--- user.php
+++ user.php
@@ -1,7 +1,7 @@
 <?php include 'wc\header.html';?>
 <?php include 'wc\navbar.html';?>
 <div class="w3-content w3-center" onload="checkAuthAndLoadContent()">
-    <h1>Welcome user! new wold here!</h1>
+    <h1>Welcome user! new world here!</h1>
     <div id="g_id_onload"
      data-client_id="YOUR_GOOGLE_CLIENT_ID"
      data-login_uri="https://your.domain/your_login_endpoint"
