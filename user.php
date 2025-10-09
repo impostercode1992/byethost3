4664dccb61483a08ca577bfd220c26a0bdce2fd8
impostercode1992 <mdshariefsalleh@gmail.com>
2025-10-09 09:36:09 +0800

    nt

--- user.php
+++ user.php
@@ -1,13 +1,13 @@
-<?php include 'wc\header.html';?>
-<?php include 'wc\navbar.html';?>
+<?php include 'wc/header.html';?>
+<?php include 'wc/navbar.html';?>
 <div class="w3-content w3-center" onload="checkAuthAndLoadContent()">
     <h1>Welcome user! new world here!</h1>
     <div id="g_id_onload"
      data-client_id="YOUR_GOOGLE_CLIENT_ID"
-     data-login_uri="https://your.domain/your_login_endpoint"
+     data-login_uri="https://norsats.my"
      data-auto_select="true">
     </div>
 </div>
 
 <script src="ga.js"></script>
-<?php include 'wc\footer.html';?>
+<?php include 'wc/footer.html';?>
