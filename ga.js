fb216d938cbf52fdd5464e1188f6a8c97a4b0a50
impostercode1992 <mdshariefsalleh@gmail.com>
2025-10-09 16:18:12 +0800

    nt

--- ga.js
+++ ga.js
@@ -163,3 +163,25 @@ function checkAuthAndLoadContent() {
     window.location.href = '/index';
   }
 }
+
+
+function w3_open() { 
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+  } else {
+    mySidebar.style.display = 'block';
+  }
+} 
+function w3_close() {
+  mySidebar.style.display = "none";
+}
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
