d320c0647568c57b85567daa7d0d49efeb202103
impostercode1992 <mdshariefsalleh@gmail.com>
2025-10-10 22:54:23 +0800

    ntf

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php include 'wc/header.html';?>
 <?php include 'wc/navbar.html';?>
-    <div class="w3-content">
-    <?php include 'wc/work.html';?>
-    </div>
+<?php include 'wc/work.html';?>    
 <?php include 'wc/footer.html';?>
