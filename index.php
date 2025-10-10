4f52c703ec850db3f7fc033a3b56bbf81f1f9465
impostercode1992 <mdshariefsalleh@gmail.com>
2025-10-10 16:15:33 +0800

    nt

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
 <?php include 'wc/header.html';?>
-  <div class="w3-margin-top w3-container w3-center" id="myNavbar">
-        <div class="w3-half w3-hide-small">
-            <?php include 'wc/navbar.html';?></div>
-            <div class="w3-half w3-hide-small">
-            <?php include 'login.html'; ?></div>
-            <a href="javascript:void(0)" class="w3-bar-item w3-button w3-left w3-hide-large w3-hide-medium" onclick="w3_open()">
-            <i class="fa fa-bars"></i>
-            </a>
-        </div>
-        <!-- Sidebar on small screens when clicking the menu icon -->
-        <nav class="w3-sidebar w3-bar-block w3-black w3-card w3-animate-left w3-hide-medium w3-hide-large" style="display:none" id="mySidebar">
-        <a href="javascript:void(0)" onclick="w3_close()" class="w3-bar-item w3-button w3-large w3-padding-16">Close ×</a>
-        <?php include 'wc/navbar.html';?>
-        </nav>
+<?php include 'wc/navbar.html';?>
     <div class="w3-content">
-    <?php include 'work.html';?>
+    <?php include 'wc/work.html';?>
     </div>
 <?php include 'wc/footer.html';?>
